81349d2478ecb29995bc1971b1c6c5db165d9caf
Bilal Taşdelen <bilaltasdelen@windowslive.com>
2023-11-30 22:14:52 -0800

    Grad moment problem fixed (?)

--- libvds_rewind/pts_to_waveform.py
+++ libvds_rewind/pts_to_waveform.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def pts_to_waveform(times, amplitudes, gradRasterTime):
-    grd = np.arange(round(min(times) / gradRasterTime)+0.5, round(max(times) / gradRasterTime)+0.5) * gradRasterTime
+    grd = np.arange(round(min(times) / gradRasterTime)+0.5, round(max(times) / gradRasterTime)+1.5) * gradRasterTime
     waveform = np.interp(grd, times, amplitudes)
     return waveform
