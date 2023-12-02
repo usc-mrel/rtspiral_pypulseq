4cc451322be6d3083f15de583766f7ef38891fa8
Bilal Taşdelen <bilaltasdelen@windowslive.com>
2023-12-02 12:14:04 -0800

    Make traj and dcf calculation a fcn.

--- write_rtspiral.py
+++ write_rtspiral.py
@@ -185,9 +185,7 @@ if params['user_settings']['detailed_rep']:
 if params['user_settings']['write_seq']:
 
     import os
-    from scipy.io import savemat
-    from scipy.signal import medfilt
-    from sigpy.mri.dcf import pipe_menon_dcf
+    from utils.traj_utils import save_traj_dcf
 
     seq.set_definition(key="FOV", value=[fov[0]*1e-2, fov[0]*1e-2, params['acquisition']['slice_thickness']*1e-3])
     seq.set_definition(key="Slice_Thickness", value=params['acquisition']['slice_thickness']*1e-3)
@@ -203,37 +201,7 @@ if params['user_settings']['write_seq']:
 
     # Export k-space trajectory
     k_traj_adc, k_traj, t_excitation, t_refocusing, t_adc = seq.calculate_kspace()
-    Nsample = int(k_traj_adc.shape[1]/n_TRs)
-    kx = k_traj_adc[0,:]
-    ky = k_traj_adc[1,:]
-    k_max = np.max(np.abs(kx + 1j * ky))
-    k = (kx / k_max) + (1j * ky / k_max)
 
-    # calculate density compensation weights using Pipe and Menon's method
-    Nsample = int(k_traj_adc.shape[1]/n_TRs)
-    w = pipe_menon_dcf(k_traj_adc[0:2, :].T, max_iter=30)
-    w = w[Nsample+1:2*Nsample+1]
-    w = w / (np.max(w))
-    w[w > 0.4] = 0.4
-    w = w / np.max(w)
-    w[int(w.shape[0]*2/3):w.shape[0]] = 1
-    w = medfilt(w, 11)
-
-    if params['user_settings']['show_plots']:
-        plt.figure()
-        plt.plot(w)
-        plt.xlabel('ADC sample')
-        plt.ylabel('|w|')
-        plt.title('DCF')
-        plt.show()
-
-    meta = {
-        'fov': fov[0],
-        'spatial_resolution': params['acquisition']['resolution'],
-        'repetitions': n_TRs,
-        'matrix_size': [fov[0]*10/res, fov[0]*10/res],
-        'pre_discard': ndiscard
-    }
-
-    traj_path = os.path.join('out_trajectory', f'{seq.signature_value}.mat')
-    savemat(traj_path, {'kx': kx, 'ky': ky, 'w' : w, 'param': meta})
+    save_traj_dcf(seq.signature_value, k_traj_adc, n_TRs, fov, res, ndiscard, params['user_settings']['show_plots'])
+    print(f'Metadata file for {seq_filename} is saved as {seq.signature_value} in out_trajectory/.')
+    
