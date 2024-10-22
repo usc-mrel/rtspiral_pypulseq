d98eb465399326579a4ca059372a7430b70a8ff4
Bilal Taşdelen <bilaltasdelen@windowslive.com>
2024-10-21 22:34:23 -0700

    Add fcn to generate encoding indices. 3D metadata save.

--- utils/traj_utils.py
+++ utils/traj_utils.py
@@ -104,3 +104,107 @@ def save_traj_analyticaldcf(filename, k_traj_adc, n_TRs, n_int, ga_rotation, fov
     traj_path = os.path.join('out_trajectory', f'{filename}.mat')
     os.makedirs("out_trajectory", exist_ok=True)
     savemat(traj_path, {'kx': kx, 'ky': ky,'kz': kz, 'w' : w, 'param': meta})
+
+def save_3Dtraj(filename, k_traj_adc, n_TRs, n_int, ga_rotation, idx, fov, res: float, adc_dwell: float = 1e-6, ndiscard: int = 10, show_plots=True):
+    Nsample = int(k_traj_adc.shape[1]/n_TRs)
+    Nsample2 = Nsample-ndiscard
+    kx = k_traj_adc[0,:]
+    ky = k_traj_adc[1,:]
+    kz = k_traj_adc[2,:]
+    kx = np.reshape(kx, (-1, Nsample)).T
+    ky = np.reshape(ky, (-1, Nsample)).T
+    kz = np.reshape(kz, (-1, Nsample)).T
+    kxx = kx[:,0]
+    kyy = ky[:,0]
+    kx = kx[ndiscard:,:]
+    ky = ky[ndiscard:,:]
+    kz = kz[ndiscard:,:]
+
+    # kx = kx[ndiscard:,0]
+    # ky = ky[ndiscard:,0]
+    gx = np.diff(np.concatenate(([0], kxx)), axis=0)/adc_dwell/42.58e6
+    gy = np.diff(np.concatenate(([0], kyy)), axis=0)/adc_dwell/42.58e6
+
+    # Analytical DCF formula
+    # 1. Hoge RD, Kwan RKS, Bruce Pike G. Density compensation functions for spiral MRI. 
+    # Magnetic Resonance in Medicine. 1997;38(1):117-128. doi:10.1002/mrm.1910380117
+
+    cosgk = np.cos(np.arctan2(kxx, kyy) - np.arctan2(gx, gy))
+    w = np.sqrt(kxx*kxx+kyy*kyy)*np.sqrt(gx*gx+gy*gy)*np.abs(cosgk)
+    w = w[ndiscard:]
+    w[-int(Nsample//2):] = w[-int(Nsample//2)] # need this to correct weird jump at the end and improve SNR
+    w = w/np.max(w)
+
+    if show_plots:
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.plot(w)
+        plt.xlabel('ADC sample')
+        plt.ylabel('|w|')
+        plt.title('DCF')
+        plt.show()
+
+    meta = {
+        'fov': fov[0],
+        'spatial_resolution': float(res),
+        'repetitions': n_TRs,
+        'interleaves': n_int,
+        'indices': idx,
+        'ga_rotation': ga_rotation,
+        'matrix_size': [fov[0]*10/res, fov[0]*10/res, 1+np.max(idx['kspace_step_2'])],
+        'pre_discard': ndiscard,
+        'dt': adc_dwell
+    }
+
+    traj_path = os.path.join('out_trajectory', f'{filename}.mat')
+    os.makedirs("out_trajectory", exist_ok=True)
+    savemat(traj_path, {'kx': kx, 'ky': ky,'kz': kz, 'w' : w, 'param': meta})
+
+
+def generate_encoding_indices(n_int, n_kz, n_rep=1, n_eco=1, kz_ordering='linear', kspace_ordering='arm'):
+    '''Generate encoding indices for a 3D multiecho acquisition.
+
+    '''
+
+    # Generate encoding indices
+    n_tr = n_int * n_kz * n_eco * n_rep
+
+    if kspace_ordering == 'arm':
+        kspace_step_1 = [ii % n_int for ii in range(n_tr)]
+        if kz_ordering == 'linear':
+            kspace_step_2 = [(ii//n_int) % n_kz for ii in range(n_tr)]
+        # TODO: Fix ping-pong ordering.
+        elif kz_ordering == 'ping-pong':
+            kspace_step_2 = []
+            c_kz = 0
+            c_kz_dir = 1
+            for ii in range(n_tr):
+                if c_kz % n_kz == 0:
+                    c_kz_dir = 1
+                elif c_kz % n_kz == n_kz - 1:
+                    c_kz_dir = -1
+                
+                kspace_step_2.append(c_kz)
+                if ii % (n_int) == 0 and ii != 0:
+                    c_kz += c_kz_dir
+
+    elif kspace_ordering == 'stack':
+        kspace_step_1 = [ii // n_kz for ii in range(n_tr)]
+        if kz_ordering == 'linear':
+            kspace_step_2 = [ii % n_kz for ii in range(n_tr)]
+        elif kz_ordering == 'ping-pong':
+            kspace_step_2 = []
+            for ii in range(n_tr):
+                if ii % (2*n_kz) < n_kz:
+                    kspace_step_2.append(ii % n_kz)
+                else:
+                    kspace_step_2.append(n_kz - 1 - ii % n_kz)
+    
+    contrast = [ii % n_eco for ii in range(n_tr)]
+
+    idx = {
+        'kspace_step_1': kspace_step_1,
+        'kspace_step_2': kspace_step_2,
+        'contrast': contrast
+    }
+    return idx
