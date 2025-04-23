2bf1f2c6c9747f164fe773c7c0e41d11c52dbb9b
pkumar <prakashk@umich.edu>
2025-04-23 09:13:32 -0700

    made changes to other write_rtspirals

--- write_rtspiral_multislice.py
+++ write_rtspiral_multislice.py
@@ -11,13 +11,13 @@ from utils import schedule_FA, load_params
 from utils.traj_utils import save_metadata
 from libspiral import vds_fixed_ro, plotgradinfo, raster_to_grad, spiralgen_design, calcgradinfo
 from libspiralutils import pts_to_waveform, design_rewinder_exact_time, round_up_to_GRT
+from librewinder.design_rewinder import design_rewinder
 from kernels.kernel_handle_preparations import kernel_handle_preparations, kernel_handle_end_preparations
 from math import ceil
 import copy
 import argparse
 import os
 import warnings
-from utils.rewinders import spiral_rewinder_m1_nayak
 
 
 # Cmd args
@@ -73,74 +73,10 @@ print(f'Number of interleaves for fully sampled trajectory: {n_int}.')
 
 t_grad, g_grad = raster_to_grad(g, spiral_sys['adc_dwell'], GRT)
 
-"""
-g_grad = rotate_z(g_grad, -np.pi/4)
-
-plotgradinfo(g_grad, GRT)
-plt.show()
-"""
-
-M = np.cumsum(g_grad, axis=0) * GRT
-t = (np.arange(1, g_grad.shape[0]+1))-0.5
-tt = (t*np.ones((2, 1))).T * GRT
-M1 = np.cumsum(g_grad * tt,  axis=0) * (GRT)
-
-grad_rew_method = params['spiral']['grad_rew_method']
-T_rew = params['spiral']['rewinder_time']
-
-if grad_rew_method == 'gropt':
-    from gropt import get_min_TE_gfix
-
-    # Method 1: GrOpt, separate optimization
-    gropt_params = {}
-    gropt_params['mode'] = 'free'
-    gropt_params['gmax'] = params['system']['max_grad']*1e-3 # [mT/m] -> [T/m]
-    gropt_params['smax'] = params['system']['max_slew']*params['spiral']['slew_ratio']
-    gropt_params['dt']   = GRT
-
-    gropt_params['moment_params']  = [[0, 0, 0, -1, -1, -M[-1,0]*1e3, 1.0e-3]]
-
-    gropt_params['gfix']  = np.array([g_grad[-1, 0]*1e-3, -99999, 0])
-
-    g_rewind_x, T = get_min_TE_gfix(gropt_params, T_rew*1e3, True)
-    g_rewind_x = g_rewind_x.T[:,0]*1e3
-
-    gropt_params['moment_params']  = [[0, 0, 0, -1, -1, -M[-1,1]*1e3, 1.0e-3]]
-
-    gropt_params['gfix']  = np.array([g_grad[-1, 1]*1e-3, -99999, 0])
-
-    g_rewind_y, T = get_min_TE_gfix(gropt_params, T_rew*1e3, True)
-    g_rewind_y = g_rewind_y.T[:,0]*1e3
-
-elif grad_rew_method == 'ext_trap_area':
-    from pypulseq.make_extended_trapezoid_area import make_extended_trapezoid_area
-
-    # Copy the system to modify slew rate to obey reduced SR of the spirals.
-    system2 = copy.deepcopy(system)
-    system2.max_slew = system.max_slew*params['spiral']['slew_ratio']
-    _,times_x,amplitudes_x = make_extended_trapezoid_area(channel='x', area=-M[-1,0]*system2.gamma*1e-3, grad_start=g_grad[-1, 0]*system2.gamma*1e-3, grad_end=0, system=system2)
-    _,times_y,amplitudes_y = make_extended_trapezoid_area(channel='y', area=-M[-1,1]*system2.gamma*1e-3, grad_start=g_grad[-1, 1]*system2.gamma*1e-3, grad_end=0, system=system2)
-
-    g_rewind_x = 1e3*pts_to_waveform(times_x, amplitudes_x, GRT)/system2.gamma
-    g_rewind_y = 1e3*pts_to_waveform(times_y, amplitudes_y, GRT)/system2.gamma
-
-elif grad_rew_method == 'exact_time':
-
-    [times_x, amplitudes_x] = design_rewinder_exact_time(g_grad[-1, 0], 0, T_rew, -M[-1,0], spiral_sys)
-    [times_y, amplitudes_y] = design_rewinder_exact_time(g_grad[-1, 1], 0, T_rew, -M[-1,1], spiral_sys)
-
-    g_rewind_x = pts_to_waveform(times_x, amplitudes_x, GRT)
-    g_rewind_y = pts_to_waveform(times_y, amplitudes_y, GRT)
-
-elif grad_rew_method == 'm1_nayak':
-    # follow Krishna's MRM 2005 method for M1-nulled trajectories
-    g_grad, g_rewind_x, g_rewind_y = spiral_rewinder_m1_nayak(g_grad, GRT, system, params['spiral']['slew_ratio'])
-
-# add zeros to the end of g_rewind_x or g_rewind_y to make them the same length (in case they are not).
-if len(g_rewind_x) > len(g_rewind_y):
-    g_rewind_y = np.concatenate((g_rewind_y, np.zeros(len(g_rewind_x) - len(g_rewind_y))))
-else:
-    g_rewind_x = np.concatenate((g_rewind_x, np.zeros(len(g_rewind_y) - len(g_rewind_x))))
+g_rewind_x, g_rewind_y = design_rewinder(g_grad, GRT, params['spiral']['rewinder_time'], system, \
+                                         slew_ratio=params['spiral']['slew_ratio'], \
+                                         grad_rew_method=params['spiral']['grad_rew_method'], \
+                                         M1_nulling=params['spiral']['M1_nulling'])
 
 # concatenate g and g_rewind, and plot.
 g_grad = np.concatenate((g_grad, np.stack([g_rewind_x[0:], g_rewind_y[0:]]).T))
